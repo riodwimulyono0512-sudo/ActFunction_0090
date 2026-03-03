5297425a7a63e48e2eacb2bf3f78e694927165fa
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-03-03 16:40:51 +0700

    Implementasi input function

--- LuasPersegi.cpp
+++ LuasPersegi.cpp
@@ -7,3 +7,18 @@ float HitungLuas(float a, float t){
     return 0.5*a*t;
 }
 
+//Implementasi input function
+void input(){
+    double a, t, luas;
+ 
+    cout << "Masukkan alas : " ;
+    cin >> a;
+
+    cout << "Masukkan tinggi : " ;
+    cin >> t;
+    
+    luas = HitungLuas(a, t);
+
+    cout << "Luas segitiga = " << luas << endl;
+}
+
