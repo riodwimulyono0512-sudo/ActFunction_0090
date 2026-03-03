7fa8a6657483337736774591f7e39eeeeea3f1f2
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-03-03 16:23:18 +0700

    Implementasi Prosedur & Fungsi

--- prak3sesi3.cpp
+++ prak3sesi3.cpp
@@ -3,4 +3,23 @@
 using namespace std;
 //Deklarasi Global
     float p,l;
-    
+
+    //Implementasi Prosedur & Fungsi
+    void Input(){
+        cout << "Masukkan Panjang : " ;
+        cin >> p;
+        cout << "Masukkan Lebar : " ;
+        cin >> l;
+    }
+
+float LuasPersegi(float x, float y){
+    return x*y;
+}
+
+int Jumlah(int a, int b, int c){
+    return a+b+c;
+}
+
+void Output (){
+    cout << "Hasilnya = " << LuasPersegi(p,l) << endl;
+}
